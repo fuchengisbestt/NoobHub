a8eb7505e3fdf9d36b6bbbf7f34bbe1b61ed359d
noobbeast <93366602+phoxknoob@users.noreply.github.com>
2023-03-09 12:05:45 +0800

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -1,9 +1,9 @@
 repeat wait() until game:IsLoaded() == true
 
 if game.PlaceId == 3351674303 then -- Driving Empire
-    loadstring(game:HttpGet("https://raw.githubusercontent.com/fuchengisbestt/NoobHub/main/MainScript/DrivingEmpire.lua"))()
+    loadstring(game:HttpGet("https://raw.githubusercontent.com/phoxknoobNoobHub/main/MainScript/DrivingEmpire.lua"))()
   
 elseif game.PlaceId == 2753915549 then -- Blox Fruit
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/fuchengisbestt/NoobHub/main/MainScript/BloxFruit.lua"))()
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/phoxknoob/NoobHub/main/MainScript/BloxFruit.lua"))()
 
 end
