2484a00fac1335a9135b2ca8c63ea813a37d9311
fuchengisbestt <93366602+fuchengisbestt@users.noreply.github.com>
2023-03-09 11:52:12 +0800

    Update MainLoader.lua

--- MainLoader.lua
+++ MainLoader.lua
@@ -3,7 +3,7 @@ repeat wait() until game:IsLoaded() == true
 if game.PlaceId == 3351674303 then -- Driving Empire
     loadstring(game:HttpGet("https://raw.githubusercontent.com/fuchengisbestt/NoobHub/main/MainScript/DrivingEmpire.lua"))()
   
-elseif game.PlaceId == 171391948 then -- Vehicle Simulator
-	loadstring(game:HttpGet("https://raw.githubusercontent.com/WindyKung/WindyWare/main/MainScript/VehicleSimulator.lua"))()
+elseif game.PlaceId == 2753915549 then -- Blox Fruit
+	loadstring(game:HttpGet("https://raw.githubusercontent.com/fuchengisbestt/NoobHub/main/MainScript/BloxFruit.lua"))()
 
 end
